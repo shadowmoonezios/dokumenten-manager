a9a1261361d30ee5bad724e8d350e660373abec1
shadowmoonezios <agushifelde@hotmail.com>
2025-09-05 21:15:38 +0300

    feat: initial commit for models/Document.js

---
+++ models/Document.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const DocumentSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  version: {
+    type: Number,
+    default: 1
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Document', DocumentSchema);
