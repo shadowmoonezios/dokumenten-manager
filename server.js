c3fc62b2a18fe1e85be18bbe2032a07cd5aab02d
shadowmoonezios <agushifelde@hotmail.com>
2025-09-05 21:19:21 +0300

    Fix typo in error message for consistency

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send('Internal Server Error');
 });
 
 // Example route
